7ebcae595d650e241064201476531421467bdf03
Nikki Thean <nikki.thean@gmail.com>
2013-11-14 16:20:33 -0500

    Add encoder.rb

---
+++ encoder.rb
@@ -0,0 +1,21 @@
+message = "The quick brown fox jumped over the lazy dog"
+
+def encode(message)
+
+  gsub_args = [
+    [/a/, "BZZZ"],
+    [/e/, "FZZZ"],
+    [/i/, "TRZZZ"],
+    [/o/, "KRZZZ"],
+    [/u/, "PRZZZ"],
+    [/ /, "YRZZZ"]
+   ]
+
+  message_history = Hash.new(message)
+
+  gsub_args.each_with_index do |args, index|
+    message_history[(index+1)] = message_history[index].send(:gsub, args[0], args[1])
+  end
+
+  puts message_history.values.last
+end
